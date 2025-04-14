da29184ce0d1b42e1a0f57b0f3f7eb8797488456
Roman Strazzanti <roman.strazzanti@gmail.com>
2025-04-14 13:17:47 +0200

    authentification jwt token

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,61 +2,23 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 class SecurityController extends AbstractController
 {
-    private JWTTokenManagerInterface $jwtManager;
-    private UserPasswordHasherInterface $passwordHasher;
-    private EntityManagerInterface $entityManager;
-
-    public function __construct(
-        JWTTokenManagerInterface $jwtManager,
-        UserPasswordHasherInterface $passwordHasher,
-        EntityManagerInterface $entityManager
-    ) {
-        $this->jwtManager = $jwtManager;
-        $this->passwordHasher = $passwordHasher;
-        $this->entityManager = $entityManager;
-    }
-
-    #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
-    public function login(Request $request): JsonResponse
+    #[Route(path: '/login', name: 'app_login', methods: ['GET'])]
+    public function showLogin(): Response
     {
-        $data = json_decode($request->getContent(), true);
-        $email = $data['email'] ?? null;
-        $password = $data['password'] ?? null;
-
-        if (!$email || !$password) {
-            return new JsonResponse(['message' => 'Email and password are required.'], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-        if (!$user || !$this->passwordHasher->isPasswordValid($user, $password)) {
-            throw new BadCredentialsException('Invalid credentials.');
-        }
-
-        if (!in_array('ROLE_ADMIN', $user->getRoles())) {
-            return new JsonResponse(['message' => 'Access Denied'], JsonResponse::HTTP_FORBIDDEN);
-        }
-
-        $token = $this->jwtManager->create($user);
-
-        return new JsonResponse(['token' => $token]);
+        return $this->render('login.html.twig');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout(): RedirectResponse //void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->redirectToRoute('app_login');
+        // throw new \LogicException('This method is intercepted by the firewall logout.');
     }
 }
